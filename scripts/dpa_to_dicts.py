9e70f63abfa4883fbc8a4a8b77dff7229296dd5d
Daniel Sullivan <sullydm@gmail.com>
2016-02-26 16:03:59 -0500

    Add scripts to turn stata `rename` commands to python dicts

---
+++ scripts/dpa_to_dicts.py
@@ -0,0 +1,53 @@
+import re
+
+dpa_path = 'clean/dataprep_a.do'
+out_path = 'scripts/varcode_dicts.txt'
+
+varnames = (
+    'match',
+    'wt',
+    'tfinc',
+    'famsize',
+    'hometype',
+    'state',
+    'sexofhead',
+    'headrace',
+    'headlabor',
+    'mrstat',
+    'wifelabor',
+    'numchild',
+    'mortmo',
+    'mort',
+    'rentmo',
+    'rent',
+    'mortpri',
+    'hvalue',
+    'stamps',
+    'stampsper',
+    'foodhome_st',
+    'foodhomeper_st',
+    'fooddel_st',
+    'fooddelper_st',
+    'foodout_st',
+    'foodoutper_st',
+    'foodhome',
+    'foodhomeper',
+    'fooddel',
+    'fooddelper',
+    'foodout',
+    'foodoutper',
+)
+
+
+with open(dpa_path, 'r') as f, open(out_path, 'w') as out:
+    for varname in varnames:
+        out.write("#{}\nvarcode = {{\n".format(varname))
+        for line in f:
+            # Match, get subgroups
+            m = re.match('rename ([ERV]{{1,2}}\d+) {}(\d{{4}})'.format(varname),
+                         line)
+            if m:
+                varcode, year = m.group(1), m.group(2)
+                out.write("    {}: '{}',\n".format(year, varcode))
+        f.seek(0)
+        out.write("}\n\n")
