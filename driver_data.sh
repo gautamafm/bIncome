49f27eda6a37fc553c64700998f8fac9ca413721
Frank McIntyre <frank.mcintyre@gmail.com>
2016-11-29 11:10:30 -0500

    fix stata command for mac

--- driver_data.sh
+++ driver_data.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+# if mac change stata command for batch
+if [ -a /Applications/Stata/StataMP.app/Contents/MacOS/StataMP ] ; then 
+    stata="/Applications/Stata/StataMP.app/Contents/MacOS/StataMP -b"
+else
+    stata=stata
+fi
+
 python -m clean.psid.io --rebuild-all
 python -m clean.bank_panels
-stata do clean/make_panel_with_weights.do
-stata do clean/make_ES_data.do
+$stata do clean/make_panel_with_weights.do
+$stata do clean/make_ES_data.do
+
